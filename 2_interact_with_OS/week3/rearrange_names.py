e8a8c37633b252db96343346bf336ba46aab713e
Igor Sheykin <igor.sheykin@gmail.com>
2022-04-07 16:39:48 +0300

    Added input for name and output

--- 2_interact_with_OS/week3/rearrange_names.py
+++ 2_interact_with_OS/week3/rearrange_names.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 import re
 
+name = input("Enter name, devided by ',': ")
+
 def rearrange_name(name):
     result = re.search(r"^(\w*), (\w*)$", name)
     if result is None:
         return name
+    return "{} {}".format(result[2], result[1])
+
+result_name = rearrange_name(name)
+print(result_name)
