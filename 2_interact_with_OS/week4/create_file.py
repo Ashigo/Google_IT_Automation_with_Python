dd1ed40c99999af2178bed351966d84b64b1c87e
Igor Sheykin <igor.sheykin@gmail.com>
2022-04-17 15:46:13 +0300

    week4 beginning

--- 2_interact_with_OS/week4/create_file.py
+++ 2_interact_with_OS/week4/create_file.py
@@ -6,7 +6,7 @@ import sys
 filename = sys.argv[1]
 
 if not os.path.exists(filename):
-    with open(filename,"w") as f:
+    with open(filename, "w") as f:
         f.write("New file created\n")
 else:
     print("Error, the file {} already exists!".format(filename))
